fffe09af0ca5f277e4f8213e832a359e325e9862
Sarah <sarahhtbarton@gmail.com>
2021-10-06 20:28:39 +0000

    adds route for print-ac

--- server.py
+++ server.py
@@ -12,5 +12,14 @@ def homepage():
     return render_template('homepage.html')
 
 
+@app.route('/print-ac', methods=['GET'])
+def get_size():
+
+    size = request.args.get('input-size') #this is a dictionary. Must be args (not form) in a GET request
+
+    return render_template('print-ac.html',
+                           size=size)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True) 
